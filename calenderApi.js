965d3787121a5e3d93d9942575c14aff75f09949
madhukar6143 <75088656+madhukar6143@users.noreply.github.com>
2022-09-11 17:15:01 +0530

    Update calenderApi.js

--- calenderApi.js
+++ calenderApi.js
@@ -60,7 +60,7 @@ try
     twoMonthAfter=twoMonthAfter.format().slice(0,19);
     todayDate=todayDate.format().slice(0,19)
    
-    userlist = await axios.get("https://clist.by/api/v2/contest/?resource="+element+"&order_by=start&username=madhukar6143&api_key=3d55d6d5e3cc483c544e8f3a57f8f14c5479d34e"&start__gte="+todayDate+"&end__lte="+twoMonthAfter+"")
+    userlist = await axios.get("https://clist.by/api/v2/contest/?resource="+element+"&order_by=start&username=madhukar6143&api_key=3d55d6d5e3cc483c544e8f3a57f8f14c5479d34e&start__gte="+todayDate+"&end__lte="+twoMonthAfter+"")
     var start = new Date()
     var end = new Date();
 
