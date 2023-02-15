71b43904314ff63cafb11b00e194c99f07ccb4e3
Madhukar <madhukar@sg-lt-233.senecaglobal.net>
2023-02-15 15:53:16 +0530

    first commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { browser, logging } from 'protractor';
 import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+  let page: AppPag;
 
   beforeEach(() => {
     page = new AppPage();
